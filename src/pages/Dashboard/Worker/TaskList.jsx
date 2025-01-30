79b512d9575291860a438d5ab1fce1e905086bc9
Sanjida <sanjida.ice.778@gmail.com>
2025-01-30 15:22:20 +0600

    add image on tasklist

--- src/pages/Dashboard/Worker/TaskList.jsx
+++ src/pages/Dashboard/Worker/TaskList.jsx
@@ -21,12 +21,13 @@ const TaskList = () => {
     <div className="w-11/12 mx-auto p-6 my-10 lg:my-20">
       <h2 className="text-2xl md:text-3xl font-bold text-center mb-6">Task List</h2>
       
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
         {tasks.map((task) => (
          <div 
          key={task._id} 
          className="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition duration-300 flex flex-col justify-between"
        >
+        <img src={task.taskImg} className="rounded-lg mb-5" alt="" />
          {/* Task Title */}
          <h3 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-2">{task.title}</h3>
        
